3d8e2367ff16e2668a51fe6edd84586e711d263f
Cloud Wu <cloudwu@gmail.com>
2025-10-18 10:02:57 +0800

    bump api version for new ltask mainthread api

--- src/version.h
+++ src/version.h
@@ -1,7 +1,7 @@
 #ifndef soluna_version_h
 #define soluna_version_h
 
-#define SOLUNA_API_VERSION 2
+#define SOLUNA_API_VERSION 3
 #ifndef SOLUNA_HASH_VERSION
 #define SOLUNA_HASH_VERSION "dev"
 #endif
