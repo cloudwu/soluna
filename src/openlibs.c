9ddcab4f805dc45410a6e84b796e333eb8a3d25e
Cloud Wu <cloudwu@gmail.com>
2025-10-29 19:42:34 +0800

    ignore env vars

--- src/openlibs.c
+++ src/openlibs.c
@@ -10,6 +10,9 @@ void soluna_embed(lua_State* L);
 
 void
 soluna_openlibs(lua_State *L) {
+	// ignore env. vars.
+    lua_pushboolean(L, 1);
+    lua_setfield(L, LUA_REGISTRYINDEX, "LUA_NOENV");
 	luaL_openlibs(L);
     soluna_embed(L);
 }
